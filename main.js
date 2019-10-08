e6c8f739ef0b8385cf83a28fcb2947148554bdf6
Rye In Seong <insung1217@gmail.com>
2019-10-08 13:54:15 +0900

    enter key 입력으로 추가하는 방식 - 미완성

--- main.js
+++ main.js
@@ -6,12 +6,28 @@ function init() {
   oBackground.style.width = "330px";
 }
 
+//enter key 입력으로 추가하는 방식 - 미완성
+// var inputText = document.getElementById("inputText");
+// inputText.focus();
+
+// inputText.onkeyup = function(keyPress) {
+//   if (keyPress.which == 13) {
+//     var itemText = inputText.value;
+//     if (!itemText || itemText === "" || itemText === " ") return false;
+
+//     addNewItem(document.getElementById("todolist"), itemText);
+//     inputText.focus();
+//     inputText.select();
+//   }
+// };
+
+//버튼을 눌러서 추가하는 방식
+//asdf
 var newBtn = document.getElementById("addBtn");
 
 newBtn.onclick = function() {
   var inputText = document.getElementById("inputText");
   var itemText = inputText.value;
-
   if (!itemText || itemText === "" || itemText === "") return false;
 
   addNewItem(document.getElementById("todolist"), itemText);
@@ -19,7 +35,6 @@ newBtn.onclick = function() {
 
 function addNewItem(list, itemText) {
   var listItem = document.createElement("li");
-
   listItem.innerText = itemText;
   list.appendChild(listItem);
 }
